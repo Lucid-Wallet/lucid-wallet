7d372e7c14c90dbc063994746fb4f2ebefc79e80
Guo Hong Wu <jorm7012@gmail.com>
2022-07-11 16:38:04 -0400

    Added default global error handler & default 404 error handling page
    co-authored-by: Guo Hong Wu <guohongwu7012@gmail.com>, Nathan Lovell <the.nathan.lovell@gmail.com>, Nevruz Olmez <nevruzolmez@gmail.com>, Travis Lovis <tlovis@mail.com>

--- index.ts
+++ index.ts
@@ -1,13 +1,43 @@
-import express, { Request, Response, Application, application } from 'express';
+"use strict";
+
+import express, { Request, Response, Application, NextFunction } from 'express';
 
 const app:Application = express();
 
 const PORT:number = 8080;
 
-app.get('/', (req: Request, res: Response):void => {
+app.get('/', (req: Request, res: Response):Response => {
 
-  res.send("Server");
+  return res.send("Server");
 });
+
+/**
+ * 404 Not found handler
+ */
+const notFoundHandler = (req: Request, res:Response, next:NextFunction):Response => {
+  return res.sendStatus(404);
+};
+
+app.use(notFoundHandler);
+
+/**
+ * Global error event handler
+ */
+
+app.use((err: any, req: Request, res: Response, next: NextFunction):Response => {
+
+  const defaultErr: { log: string, status: number, message: { err: string } } = {
+    log: `Express error handler caught unknown middleware error ${err}`,
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 
 app.listen(PORT, ():void => {
   console.log(`Listening on PORT ${PORT}`);
